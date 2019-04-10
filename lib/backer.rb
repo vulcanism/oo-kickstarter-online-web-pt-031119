98af325f1ab58a73e1db7ddf50ff78b5a399e9bb
Savannah C <sgcorrell@gmail.com>
2019-04-10 18:57:22 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer
+  attr_reader :backed_projects, :name
+  
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+  
+end
