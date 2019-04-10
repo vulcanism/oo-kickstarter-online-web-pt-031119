fdd01907d986a0ed304b1e2ecc35521c5a2d3398
Savannah C <sgcorrell@gmail.com>
2019-04-10 18:54:04 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,9 @@
 class Backer
   attr_reader :backed_projects, :name
   
-  def initialize
+  def initialize(name)
     @backed_projects = []
+    @name = name
   end
   
   def back_project(project)
