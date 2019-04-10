023561bb58ded57018ad6db1b2efc8471f3a2f00
Savannah C <sgcorrell@gmail.com>
2019-04-10 18:54:26 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,9 @@
 class Project
-  attr_reader :backers
+  attr_reader :backers, :title
   
-  def initialize(name)
+  def initialize(title)
     @backers = []
+    @title = title
   end
   
   def add_backer(backer)
